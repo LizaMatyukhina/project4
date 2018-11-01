96796f5fd279c29a1be923d36330e0ea151980ba
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2018-11-01 15:46:56 +0700

    start

---
+++ project4.py
@@ -0,0 +1,9 @@
+import random
+
+print('Загадано четырехбуквенное слово из букв А, E, Н, О, С, Т.')
+print('Отгадай!')
+
+letters = ['А', 'Е', 'Н', 'О', 'С', 'Т']
+a = []
+word = None
+smn_place = None
